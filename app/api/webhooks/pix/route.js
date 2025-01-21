c8574cdd3ed676f90da029677333a3182505fb3e
Ryan varela <108085059+varelaprogramador@users.noreply.github.com>
2025-01-20 22:58:29 -0300

    api console.log

--- app/api/webhooks/pix/route.js
+++ app/api/webhooks/pix/route.js
@@ -3,6 +3,7 @@ require('dotenv').config();
 export async function GET(req) {
   try {
     // Lógica do seu endpoint GET (se necessário)
+    console.log(req.json())
     return new Response(JSON.stringify({ message: "GET request successful" }), {
       status: 200,
       headers: {
@@ -24,6 +25,7 @@ export async function POST(req) {
   try {
     // Lógica do seu endpoint POST (se necessário)
     const data = await req.json(); // Para acessar o corpo da requisição, se houver
+    console.log(data)
     return new Response(JSON.stringify({ message: "POST request successful", data }), {
       status: 200,
       headers: {
