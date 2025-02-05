3f10575086301e09e88fcee97acfe8772a321989
Ryan varela <108085059+varelaprogramador@users.noreply.github.com>
2025-02-05 17:18:08 -0300

    commit

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -18,6 +18,7 @@ const eslintConfig = [
       "no-require-imports": "off",
       "@typescript-eslint/no-unused-vars": "off",
       "@typescript-eslint/no-require-imports": "off",
+      "@typescript-eslint/no-explicit-any" : "off"
     },
   },
 ];
