5e6fd7966dc5f115df5b98814133f8ccc710eff6
Ryan varela <108085059+varelaprogramador@users.noreply.github.com>
2025-04-23 16:00:10 -0300

    feat: remover rota de documentação da API para idiomas

--- lib/supabase/server.ts
+++ lib/supabase/server.ts
@@ -13,14 +13,14 @@ export function createServerSupabaseClient() {
 
   return createServerClient(supabaseUrl, supabaseKey, {
     cookies: {
-      get(name: string) {
-        return cookieStore.get(name)?.value;
+      async get(name: string) {
+        return (await cookieStore).get(name)?.value;
       },
-      set(name: string, value: string, options: any) {
-        cookieStore.set(name, value, options);
+      async set(name: string, value: string, options: any) {
+        (await cookieStore).set(name, value, options);
       },
-      remove(name: string, options: any) {
-        cookieStore.set(name, "", { ...options, maxAge: 0 });
+      async remove(name: string, options: any) {
+        (await cookieStore).set(name, "", { ...options, maxAge: 0 });
       },
     },
   });
